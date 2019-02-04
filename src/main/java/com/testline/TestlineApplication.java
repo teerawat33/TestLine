ac62de4bca191808c8ab32c3a5cd4ee9fd7e1b30
teerawat33 <teerawat@antinode.co.th>
2019-02-04 16:38:48 +0700

    start

---
+++ src/main/java/com/testline/TestlineApplication.java
@@ -0,0 +1,14 @@
+package com.testline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestlineApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/testline/TestlineApplicationTests.java
@@ -0,0 +1,17 @@
+package com.testline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestlineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
