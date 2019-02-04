5a083a58e8513404fbc3684b45d74ba865104050
teerawat33 <teerawat@antinode.co.th>
2019-02-04 17:02:56 +0700

    update

--- src/main/java/com/testline/TestlineApplication.java
+++ src/main/java/com/testline/TestlineApplication.java
@@ -1,5 +1,10 @@
 package com.testline;
 
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,13 @@ public class TestlineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestlineApplication.class, args);
+	}
+
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+		System.out.println("event: " + e);
+		TextMessageContent message = e.getMessage();
+		return new TextMessage(message.getText());
 	}
 
 }
